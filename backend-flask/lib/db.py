a8c3cfc58ea8e1635210d7b1d7b8ed48c5f229de
Bhavneet Kaur Kaberwal <bkkaberwal@gmail.com>
2023-05-10 00:57:24 +0000

    finish creating utility scripts  working with DynamoDB

--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -43,6 +43,21 @@ class Db:
     except Exception as error:
       self.print_sql_err(error)
 
+  def print_sql(self,title,sql,params={}):
+    cyan = '\033[96m'
+    no_color = '\033[0m'
+    print(f'{cyan} SQL STATEMENT-[{title}]------{no_color}')
+    print(sql,params)
+
+  # when we want to return a a single value
+  def query_value(self,sql,params={}):
+    self.print_sql('value',sql,params)
+    with self.pool.connection() as conn:
+      with conn.cursor() as cur:
+        cur.execute(sql,params)
+        json = cur.fetchone()
+        return json[0]
+
   # when we want to return an array of json objects
   def query_array_json(self, sql, params={}):
     wrapped_sql = self.query_wrap_array(sql)
